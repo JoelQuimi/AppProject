c4deaf060ac92d0d56c037f2d07c55edb512b84d
Joel Quimis <quimisjoedl@gmail.com>
2025-07-06 21:46:47 -0500

    DB en la nube

--- includes/database.php
+++ includes/database.php
@@ -1,6 +1,15 @@
 <?php
 
-$db = mysqli_connect('localhost', 'root', 'JOELQ2003', 'app_salon');
+// $db = mysqli_connect('localhost', 'root', 'JOELQ2003', 'app_salon');
+
+
+// if (!$db) {
+//     echo "Error: No se pudo conectar a MySQL.";
+//     echo "errno de depuración: " . mysqli_connect_errno();
+//     echo "error de depuración: " . mysqli_connect_error();
+//     exit;
+// }   
+$db = mysqli_connect('b6ihkyftwac2plxe6gqw-mysql.services.clever-cloud.com', 'uvz3pkkjxfdai6mt', '7HqUqzj5Ehr5wyEqesNJ', 'b6ihkyftwac2plxe6gqw');
 
 
 if (!$db) {
